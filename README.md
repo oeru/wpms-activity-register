b5f40d7047294c24ba04a501c1bce9b0f2df6b31
Dave Lane <dave@davelane.nz>
2017-05-09 10:03:11 +1200

    initial commit of code, fleshed out README

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # wpms-activity-register
-Record per-site statistics related to (multi-site) sites and site users.
+
+Our goal is to record per-site and per-site-user statistics in a WordPress Multisite configuration so that we can quantify user participation and eventually use it as a way to offer reports on a per-site, per-user, and per-site-per-user basis, and automated recognition (e.g. microcourse open badges).
+
+This plug-in creates a new registry table for storing activity data.
+
+This plug-in is the work of the Open Education Resource Foundation on behalf of the OERuniversitas (http://oeru.org). We are using WordPress to deliver open courses (using exclusively openly licensed (CC-By or CC-By-SA) course materials) assembled by our partners on one of our other initiatives: http://wikieducator.org You (and your institution) are also welcome to use these materials, and are invited to consider becoming OERu partners!
---
+++ activity-register.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * @package Activity Register
+ */
+/*
+Plugin Name: MultiSite Activity Register
+Plugin URI: http://github.com/oeru/wpms-activity-register
+Description: Record per-site multisite statistics related to site CRUD and site users to allow assessing participation over time.
+Version: 0.0.1
+Author: Dave Lane
+Author URI: https://davelane.nz
+License: GPLv2 or later
+Network: true
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+define('ACTREG_VERSION', '0.0.1');
+// the path to this file
+define('ACTREG_FILE', __FILE__);
+// absolute URL for this plugin, including site name, e.g.
+// https://sitename.nz/wp-content/plugins/
+define('ACTREG_URL', plugins_url("/", __FILE__));
+// absolute server path to this plugin
+define('ACTREG_PATH', plugin_dir_path(__FILE__));
+// module details
+define('ACTREG_SLUG', 'actreg_sync');
+define('ACTREG_TITLE', 'Activity Register');
+define('ACTREG_MENU', 'Activity Register');
+// admin details
+define('ACTREG_ADMIN_SLUG', 'actreg_settings');
+define('ACTREG_ADMIN_TITLE', 'Activity Register Settings');
+define('ACTREG_ADMIN_MENU', 'Act Reg Settings');
+// turn on debugging with true, off with false
+define('ACTREG_DEBUG', true);
+define('ACTREG_TABLE', wp_activity_register)
+
+// include Activity Register API and Auth code
+include_once ACTREG_PATH . '/vendor/autoload.php';
+// the rest of the app
+require ACTREG_PATH . '/includes/actreg.php';
+
+/**
+ * Start the plugin only if in Admin side and if site is Multisite
+ * see http://stackoverflow.com/questions/13960514/how-to-adapt-my-plugin-to-multisite/
+ */
+if (is_admin() && is_multisite()) {
+    add_action('plugins_loaded',
+        array(ActivityRegister::get_instance(), 'init')
+    );
+}
